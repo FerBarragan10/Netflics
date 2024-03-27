318e3365ec1add53d5b193cf81efdf2e863b97e5
Fer-Barragan <barragan.fer10@gmail.com>
2024-03-27 09:55:38 -0300

    agregado de rutas al app.routes (se agrego planes)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { PerfilesComponent } from './perfiles/perfiles.component';
+import { PlanesComponent } from './planes/planes.component';
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
     { path: 'create', component: CreateAccountComponent },
     { path: 'perfiles', component: PerfilesComponent},
+    { path: 'planes', component: PlanesComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' }
 
 ];
