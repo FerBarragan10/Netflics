62258cdc99e1d7444d09006009a30f4e3703be41
Fer-Barragan <barragan.fer10@gmail.com>
2024-03-31 15:58:17 -0300

    mi cuenta terminado

--- src/app/components/mi-cuenta/mi-cuenta.component.ts
+++ src/app/components/mi-cuenta/mi-cuenta.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { InfoPerfilComponent } from '../../shared/info-perfil/info-perfil.component';
 import { DetailAccountComponent } from '../../shared/detail-account/detail-account.component';
 import { InfoPlanComponent } from '../../shared/info-plan/info-plan.component';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-mi-cuenta',
   standalone: true,
-  imports: [InfoPerfilComponent,DetailAccountComponent,InfoPlanComponent],
+  imports: [InfoPerfilComponent,DetailAccountComponent,InfoPlanComponent,NavbarComponent],
   templateUrl: './mi-cuenta.component.html',
   styleUrl: './mi-cuenta.component.scss'
 })
